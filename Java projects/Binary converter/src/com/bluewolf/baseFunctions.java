71b7cc5dfad8df54788b1313e9e511427b569db0
Bluewolf279 <Bluewolf279@users.noreply.github.com>
2015-08-06 19:47:44 +1000

    Added Split Number which splits a string after a number of characters

--- Java projects/Binary converter/src/com/bluewolf/baseFunctions.java
+++ Java projects/Binary converter/src/com/bluewolf/baseFunctions.java
@@ -1,5 +1,9 @@
 package com.bluewolf;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matt on 28/07/2015.
  * Class use: This class is used for basic math functions.
@@ -10,6 +14,30 @@ public class baseFunctions {
     }
     public static int toRadians(int i){
         return (int) (i * (Math.PI / 180));
+    }
+    public void printTofile(String text, boolean lines) {
+        Writer writer = null;
+
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream("filename.txt"), "utf-8"));
+            writer.write("Something");
+        } catch (IOException ex) {
+            // report
+        } finally {
+            try {writer.close();} catch (Exception ex) {/*ignore*/}
+        }
+    }
+    public static List<String> splitNumber(String text, int i) {
+        //part stolen from http://stackoverflow.com/questions/9276639/java-how-to-split-a-string-by-a-number-of-characters
+        List<String> strings = new ArrayList<>();
+        int index = 0;
+        while (index < text.length()) {
+            strings.add(text.substring(index, Math.min(index + i,text.length())));
+            index += i;
+        }
+        //
+        return strings;
     }
 
 }
