9cb37789a86003d20eec1f24d47ab3c50b88b8dc
Bluewolf279 <Bluewolf279@users.noreply.github.com>
2015-08-06 16:24:43 +1000

    Added previous work
    6/8/15

---
+++ Java projects/Binary converter/src/com/bluewolf/baseFunctions.java
@@ -0,0 +1,15 @@
+package com.bluewolf;
+
+/**
+ * Created by Matt on 28/07/2015.
+ * Class use: This class is used for basic math functions.
+ */
+public class baseFunctions {
+    public static int toDegrees(int i) {
+        return (int) (i * (Math.PI / 180));
+    }
+    public static int toRadians(int i){
+        return (int) (i * (180 / Math.PI));
+    }
+
+}
