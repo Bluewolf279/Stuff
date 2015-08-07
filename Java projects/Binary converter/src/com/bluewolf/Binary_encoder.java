33ef311afdd5c5a6b33e86bc2c3bffdec8f8fc32
Bluewolf279 <tracymatt2@hotmail.co.uk>
2015-08-07 12:15:18 +1000

    Added decodeBinary Added printFile

--- Java projects/Binary converter/src/com/bluewolf/Binary_encoder.java
+++ Java projects/Binary converter/src/com/bluewolf/Binary_encoder.java
@@ -1,5 +1,7 @@
 package com.bluewolf;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -176,4 +178,209 @@ public class Binary_encoder {
         }
         return out;
     }
+    public static String fromBinary(String code) {
+        String text ="";
+        List<String> subtext = new ArrayList<>();
+        int index = 0;
+        while (index < code.length()) {
+            subtext.add(code.substring(index, Math.min(index + 8,code.length())));
+            index += 8;
+        }
+        for (String texts : subtext) {
+            if (Objects.equals(texts,"01000001")) {
+                text +="A";
+            }else if (Objects.equals(texts,"01000010")) {
+                text +="B";
+            }else if (Objects.equals(texts,"01000011")) {
+                text +="C";
+            }else if (Objects.equals(texts,"01000100")) {
+                text +="D";
+            }else if (Objects.equals(texts,"01000101")) {
+                text +="E";
+            }else if (Objects.equals(texts,"01000110")) {
+                text +="F";
+            }else if (Objects.equals(texts,"01000111")) {
+                text +="G";
+            }else if (Objects.equals(texts,"01001000")) {
+                text +="H";
+            }else if (Objects.equals(texts,"01001001")) {
+                text +="I";
+            }else if (Objects.equals(texts,"01001010")) {
+                text +="J";
+            }else if (Objects.equals(texts,"01001011")) {
+                text +="K";
+            }else if (Objects.equals(texts,"01001100")) {
+                text +="L";
+            }else if (Objects.equals(texts,"01001101")) {
+                text +="M";
+            }else if (Objects.equals(texts,"01001110")) {
+                text +="N";
+            }else if (Objects.equals(texts,"01001111")) {
+                text +="O";
+            }else if (Objects.equals(texts,"01010000")) {
+                text +="P";
+            }else if (Objects.equals(texts,"01010001")) {
+                text +="Q";
+            }else if (Objects.equals(texts,"01010010")) {
+                text +="R";
+            }else if (Objects.equals(texts,"01010011")) {
+                text +="S";
+            }else if (Objects.equals(texts,"01010100")) {
+                text +="T";
+            }else if (Objects.equals(texts,"01010101")) {
+                text +="U";
+            }else if (Objects.equals(texts,"01010110")) {
+                text +="V";
+            }else if (Objects.equals(texts,"01010111")) {
+                text +="W";
+            }else if (Objects.equals(texts,"01011000")) {
+                text +="X";
+            }else if (Objects.equals(texts,"01011001")) {
+                text +="Y";
+            }else if (Objects.equals(texts,"01011010")) {
+                text +="Z";
+            }else if (Objects.equals(texts,"10100000")) {
+                text +=" ";
+            }else if (Objects.equals(texts,"01100001")) {
+                text +="a";
+            }else if (Objects.equals(texts,"01100010")) {
+                text +="b";
+            }else if (Objects.equals(texts,"01100011")) {
+                text +="c";
+            }else if (Objects.equals(texts,"01100100")) {
+                text +="d";
+            }else if (Objects.equals(texts,"01100101")) {
+                text +="e";
+            }else if (Objects.equals(texts,"01100110")) {
+                text +="f";
+            }else if (Objects.equals(texts,"01100111")) {
+                text +="g";
+            }else if (Objects.equals(texts,"01101000")) {
+                text +="h";
+            }else if (Objects.equals(texts,"01101001")) {
+                text +="i";
+            }else if (Objects.equals(texts,"01101010")) {
+                text +="j";
+            }else if (Objects.equals(texts,"01101011")) {
+                text +="k";
+            }else if (Objects.equals(texts,"01101100")) {
+                text +="l";
+            }else if (Objects.equals(texts,"01101101")) {
+                text +="m";
+            }else if (Objects.equals(texts,"01101110")) {
+                text +="n";
+            }else if (Objects.equals(texts,"01101111")) {
+                text +="o";
+            }else if (Objects.equals(texts,"01110000")) {
+                text +="p";
+            }else if (Objects.equals(texts,"01110001")) {
+                text +="q";
+            }else if (Objects.equals(texts,"01110010")) {
+                text +="r";
+            }else if (Objects.equals(texts,"01110011")) {
+                text +="s";
+            }else if (Objects.equals(texts,"01110100")) {
+                text +="t";
+            }else if (Objects.equals(texts,"01110101")) {
+                text +="u";
+            }else if (Objects.equals(texts,"01110110")) {
+                text +="v";
+            }else if (Objects.equals(texts,"01110111")) {
+                text +="w";
+            }else if (Objects.equals(texts,"01111000")) {
+                text +="x";
+            }else if (Objects.equals(texts,"01111001")) {
+                text +="y";
+            }else if (Objects.equals(texts,"01111010")) {
+                text +="z";
+            }else if (Objects.equals(texts,"00110000")) {
+                text +="0";
+            }else if (Objects.equals(texts,"00110001")) {
+                text +="1";
+            }else if (Objects.equals(texts,"00110010")) {
+                text +="2";
+            }else if (Objects.equals(texts,"00110011")) {
+                text +="3";
+            }else if (Objects.equals(texts,"00110100")) {
+                text +="4";
+            }else if (Objects.equals(texts,"00110101")) {
+                text +="5";
+            }else if (Objects.equals(texts,"00110110")) {
+                text +="6";
+            }else if (Objects.equals(texts,"00110111")) {
+                text +="7";
+            }else if (Objects.equals(texts,"00111000")) {
+                text +="8";
+            }else if (Objects.equals(texts,"00111001")) {
+                text +="9";
+            }else if (Objects.equals(texts,"00111010")) {
+                text +=":";
+            }else if (Objects.equals(texts,"00111011")) {
+                text +=";";
+            }else if (Objects.equals(texts,"00100010")) {
+                text +='"';
+            }else if (Objects.equals(texts,"00100111")) {
+                text +="'";
+            }else if (Objects.equals(texts,"00111101")) {
+                text +="=";
+            }else if (Objects.equals(texts,"00101011")) {
+                text +="+";
+            }else if (Objects.equals(texts,"00111111")) {
+                text +="?";
+            }else if (Objects.equals(texts,"00101110")) {
+                text +=".";
+            }else if (Objects.equals(texts,"00101100")) {
+                text +=",";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }else if (Objects.equals(texts,"")) {
+                text +="";
+            }
+
+        }
+        return text;
+
+    }
+
+
 }
--- Java projects/Binary converter/src/com/bluewolf/baseFunctions.java
+++ Java projects/Binary converter/src/com/bluewolf/baseFunctions.java
@@ -1,5 +1,9 @@
 package com.bluewolf;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matt on 28/07/2015.
  * Class use: This class is used for basic math functions.
@@ -12,4 +16,30 @@ public class baseFunctions {
         return (int) (i * (Math.PI / 180));
     }
 
-}
+    public void printFile(String text, String fileName,int charPerline) {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(fileName+ ".txt"), "utf-8"))) {
+            if (text.length() > charPerline) {
+
+                List<String> subtext = new ArrayList<String>();
+                int index = 0;
+                while (index < text.length()) {
+                    subtext.add(text.substring(index, Math.min(index + charPerline,text.length())));
+                    index += charPerline;
+                }
+                writer.write(subtext.get(0));
+                for (int i = 1;text.length()>144*i;i++) {
+                    writer.write("\n"+subtext.get(i));
+                }
+            }
+            else {
+                writer.write(text);
+            }
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+        }
